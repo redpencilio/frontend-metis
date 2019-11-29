6bcc16a37bcbed383c11a598d0fbea577374ded5
Aad Versteden <madnificent@gmail.com>
2019-11-29 22:06:11 +0100

    Adding component to render values with short form
    The short form currently comes from preflabel.org.  We intend to
    extend this with the information available to the user from their own
    triplestore too.  The general approach is the same so the results are
    expected to be similar.

---
+++ app/components/metis/display-uri.js
@@ -0,0 +1,49 @@
+import { tracked } from '@glimmer/tracking';
+import Component from '@glimmer/component';
+import fetch from 'fetch';
+
+export default class MetisDisplayUriComponent extends Component {
+  @tracked externalPreflabel = null
+  @tracked internalPreflabel = null;
+  @tracked showShort = true;
+
+  constructor(){
+    super(...arguments);
+    if( this.args.uri ){
+      this.fetchPreflabels();
+    }
+  }
+
+  didReceiveAttrs(){
+    console.log("Received attrs");
+    this.fetchPreflabels();
+  }
+
+  async fetchPreflabels(){
+    if( this.args.uri ) {
+      const base = "http://preflabel.org/api/v1/label/";
+      const fetchUrl = new URL(`${base}${encodeURIComponent( this.args.uri )}`);
+      const request = await fetch( fetchUrl );
+      const value = await request.text();
+      if( request.status == 200 ) {
+        this.externalPreflabel = value;
+      } else {
+        this.externalPreflabel = null;
+      }
+    } else {
+      this.externalPreflabel = null;
+    }
+  }
+
+  get label() {
+    if( this.hasShort && this.showShort ) {
+      return this.externalPreflabel;
+    } else {
+      return this.args.uri;
+    }
+  }
+
+  get hasShort() {
+    return this.externalPreflabel;
+  }
+}
