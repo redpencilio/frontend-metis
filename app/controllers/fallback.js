5a330fb9ae73be01c7a70f568d410c1dec6c96bb
Miet Claes <miet@miet.be>
2019-12-20 12:08:18 +0100

    voeg blank pagina's toe en verrijk data

--- app/controllers/fallback.js
+++ app/controllers/fallback.js
@@ -1,5 +1,9 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default class FallbackController extends Controller {
-  
+	@computed('model.triples.directed', 'model.triples.inverse')
+  get isEmpty() {
+  	return this.model.triples.directed.length == 0 && this.model.triples.inverse.length == 0;
+  }
 }
